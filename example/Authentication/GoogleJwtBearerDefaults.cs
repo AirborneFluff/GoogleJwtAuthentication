ac1362654bf0f69535656b1979a71ef491518bd4
Fluff <proffitt.m.asidrain@gmail.com>
2022-12-06 20:52:59 +0000

    Added Example

---
+++ example/Authentication/GoogleJwtBearerDefaults.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace API.Authentication;
+
+public static class GoogleJwtBearerDefaults
+{
+    /// <summary>
+    /// The default authority for Google authentication.
+    /// </summary>
+    public static readonly string Authority = "https://accounts.google.com";
+
+    /// <summary>
+    /// The default authentication scheme for Google authentication.
+    /// </summary>
+    public static readonly string AuthenticationScheme = "Google." + JwtBearerDefaults.AuthenticationScheme;
+}
---
+++ example/Program.cs
@@ -0,0 +1,45 @@
+using API.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(o => o.UseGoogle(configuration["Authentication:Google:ClientId"]));
+
+/* To use config...
+ * dotnet user-secrets init
+ * dotnet user-secrets set "Authentication:Google:ClientId" "<ClientIdFromGoogleApp>"
+ */
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
